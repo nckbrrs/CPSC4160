ce0c98be9d5042af7f7e32536ce4dade01ab8483
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2018-02-22 16:30:23 -0500

    p3 done

--- project3/tracker/multiSprite.h
+++ project3/tracker/multiSprite.h
@@ -13,16 +13,16 @@ public:
   virtual void draw() const;
   virtual void update(Uint32 ticks);
 
-  virtual const Image* getImage() const { 
-    return images[currentFrame]; 
+  virtual const Image* getImage() const {
+    return images[currentFrame];
   }
-  int getScaledWidth()  const { 
-    return getScale()*images[currentFrame]->getWidth(); 
-  } 
-  int getScaledHeight()  const { 
-    return getScale()*images[currentFrame]->getHeight(); 
-  } 
-  virtual const SDL_Surface* getSurface() const { 
+  int getScaledWidth()  const {
+    return getScale()*images[currentFrame]->getWidth();
+  }
+  int getScaledHeight()  const {
+    return getScale()*images[currentFrame]->getHeight();
+  }
+  virtual const SDL_Surface* getSurface() const {
     return images[currentFrame]->getSurface();
   }
 
