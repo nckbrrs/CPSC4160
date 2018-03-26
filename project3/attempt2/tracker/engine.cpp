890dadf9eef343f9ae9615ef53a20682e423faee
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2018-03-26 00:02:42 -0400

    split project3 into attempts 1 and 2

---
+++ project3/attempt2/tracker/engine.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <random>
+#include <iomanip>
+#include "sprite.h"
+#include "multiSprite.h"
+#include "twoWayMultiSprite.h"
+#include "gamedata.h"
+#include "engine.h"
+#include "frameGenerator.h"
+
+Engine::~Engine() {
+  sprites.clear();
+  std::cout << "Terminating program" << std::endl;
+}
+
+Engine::Engine() :
+  rc( RenderContext::getInstance() ),
+  io( IoMod::getInstance() ),
+  clock( Clock::getInstance() ),
+  renderer( rc->getRenderer() ),
+  sky("sky", Gamedata::getInstance().getXmlInt("sky/factor") ),
+  backMtns("backMtns", Gamedata::getInstance().getXmlInt("backMtns/factor") ),
+  frontMtns("frontMtns", Gamedata::getInstance().getXmlInt("frontMtns/factor") ),
+  road("road", Gamedata::getInstance().getXmlInt("road/factor") ),
+  viewport( Viewport::getInstance() ),
+  sprites(),
+  currentSprite(0),
+  makeVideo( false )
+{
+  for (int i = 0; i < Gamedata::getInstance().getXmlInt("numStickmans"); i++) {
+    sprites.push_back(new TwoWayMultiSprite("Stickman"));
+  }
+  for (int i = 0; i < Gamedata::getInstance().getXmlInt("numClouds"); i++) {
+    sprites.push_back(new Sprite("Cloud"));
+  }
+  Viewport::getInstance().setObjectToTrack(sprites[0]);
+  std::cout << "Loading complete" << std::endl;
+}
+
+void Engine::draw() const {
+  sky.draw();
+  backMtns.draw();
+  frontMtns.draw();
+  road.draw();
+
+  for (int i = 0; i < (int)sprites.size(); i++) {
+    sprites[i]->draw();
+  }
+
+  viewport.draw();
+  SDL_RenderPresent(renderer);
+}
+
+void Engine::update(Uint32 ticks) {
+  for (int i = 0; i < (int)sprites.size(); i++) {
+    sprites[i]->update(ticks);
+  }
+
+  sky.update();
+  backMtns.update();
+  frontMtns.update();
+  road.update();
+  viewport.update(); // always update viewport last
+}
+
+void Engine::switchSprite(){
+  ++currentSprite;
+  currentSprite = currentSprite % sprites.size();
+  Viewport::getInstance().setObjectToTrack(sprites[currentSprite]);
+}
+
+void Engine::play() {
+  SDL_Event event;
+  const Uint8* keystate;
+  bool done = false;
+  Uint32 ticks = clock.getElapsedTicks();
+  FrameGenerator frameGen;
+
+  while ( !done ) {
+    // The next loop polls for events, guarding against key bounce:
+    while ( SDL_PollEvent(&event) ) {
+      keystate = SDL_GetKeyboardState(NULL);
+      if (event.type ==  SDL_QUIT) { done = true; break; }
+      if(event.type == SDL_KEYDOWN) {
+        if (keystate[SDL_SCANCODE_ESCAPE] || keystate[SDL_SCANCODE_Q]) {
+          done = true;
+          break;
+        }
+        if ( keystate[SDL_SCANCODE_P] ) {
+          if ( clock.isPaused() ) clock.unpause();
+          else clock.pause();
+        }
+        if ( keystate[SDL_SCANCODE_T] ) {
+          switchSprite();
+        }
+        if (keystate[SDL_SCANCODE_F4] && !makeVideo) {
+          std::cout << "Initiating frame capture" << std::endl;
+          makeVideo = true;
+        }
+        else if (keystate[SDL_SCANCODE_F4] && makeVideo) {
+          std::cout << "Terminating frame capture" << std::endl;
+          makeVideo = false;
+        }
+      }
+    }
+
+    // In this section of the event loop we allow key bounce:
+    ticks = clock.getElapsedTicks();
+    if ( ticks > 0 ) {
+      clock.incrFrame();
+      draw();
+      update(ticks);
+      if ( makeVideo ) {
+        frameGen.makeFrame();
+      }
+    }
+  }
+}
---
+++ project3/attempt2/tracker/main.cpp
@@ -0,0 +1,21 @@
+// Brian Malloy        Data-Driven Object oriented Game Construction
+#include "engine.h"
+
+/* no longer needed since converted to Meyers singleton
+ImageFactory*  ImageFactory::instance = NULL;
+*/
+
+RenderContext* RenderContext::instance = NULL;
+
+int main(int, char*[]) {
+   try {
+      Engine engine;
+      engine.play();
+      delete RenderContext::getInstance();
+   }
+   catch (const string& msg) { std::cout << msg << std::endl; }
+   catch (...) {
+      std::cout << "Oops, someone threw an exception!" << std::endl;
+   }
+   return 0;
+}
